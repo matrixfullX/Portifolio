694cd8dae873e04d90b254b01cbd4d42a8f4b6fb
MatrixfullX <vanderleylima2809@gmail.com>
2025-09-26 17:28:26 -0300

    estilizando a sidebar

--- src/components/Paragrafo/styles.ts
+++ src/components/Paragrafo/styles.ts
@@ -1,7 +1,10 @@
 import styled from 'styled-components'
 
-export const P = styled.p<{ $tipo: 'principal' | 'secundario' }>`
-  font-size: 14px;
+export const P = styled.p<{
+  $tipo: 'principal' | 'secundario'
+  $fontSize?: number
+}>`
   color: ${({ $tipo }) => ($tipo === 'principal' ? '#282A35' : '#949494')};
   line-height: 22px;
+  font-size: ${({ $fontSize }) => ($fontSize ? `${$fontSize}px` : '14px')};
 `
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,20 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SideBarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/matrixfullX.png" />
-    <Titulo fontSize={20}>Matrix</Titulo>
+    <SideBarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Matrix</Titulo>
+      <Paragrafo fontSize={16} tipo="secundario">
+        matrixfullX
+      </Paragrafo>
+      <Descricao $tipo="principal">Engenheiro Front End</Descricao>
+      <BotaoTema>Trocar Tema</BotaoTema>
+    </SideBarContainer>
   </aside>
 )
 
